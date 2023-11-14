854c7f5e00367c40d530651c59bb49f3467a3af5
Karolina Ryzka <ryzkakarolina@gmail.com>
2023-11-13 22:08:31 -0600

    new version

--- huggingface_ptm_forking/originalBaseModel/test2.py
+++ huggingface_ptm_forking/originalBaseModel/test2.py
@@ -2,25 +2,30 @@ import json
 import requests
 import re
 import markdown
+from urllib.parse import urlparse
 import os
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-def getlinks(fileContents: str):
+def getLink(fileContents: str):
     #parse content
     html = markdown.markdown(fileContents)
 
-    links = re.findall(r'<a\s+(?:[^>]*?\s+)?href="([^"]*)"', html)
-
-    return links
-
-def identifyBaseModel(links: list):
+    match = re.search(r'<a\s+(?:[^>]*?\s+)?href="([^"]*)"', html)
     
-    return "fuck"
+    if match:
+        URL = match.group(1)
+        segments = urlparse(URL).path.split("/")
+        baseModel = segments[-1] if segments else URL
+        return baseModel
+    else:
+        return None
+
+
 
 def traverse_directory(rootDirectory, outputFile):
     results = {}
-    #models set up of PTMTorrent
+    #models set up of PeatMOSS
     for folderName, subFolders, filenames in os.walk(rootDirectory):
         for file in filenames:
             if(file.endswith(".md")):
@@ -28,21 +33,29 @@ def traverse_directory(rootDirectory, outputFile):
 
                 with open(filePath, 'r') as file:
                     fileContents = file.read()
-
-                #call function here
+                result = getLink(fileContents)
+                results[file] = result
+                
     
     #place result in json file
     with open(outputFile, 'w') as jsonFile:
         json.dump(results, jsonFile, indent=2)
 
+def main():
+    # filePath = "/Users/karolinaryzka/Documents/huggingface-ptm-forking/huggingface-ptm-forking/huggingface_ptm_forking/originalBaseModel/README.md"
+    # with open(filePath, 'r') as file:
+    #     fileContents = file.read()
+    # result = getLink(fileContents)
+    # print(result)
+    rootDirectory = "/Users/karolinaryzka/Documents/huggingface-ptm-forking/huggingface-ptm-forking/huggingface_ptm_forking/originalBaseModel/Dataset"
+    outputFile = "output.json"
+    traverse_directory(rootDirectory, outputFile)
+    
+
 if __name__ == "__main__":
-    filePath = "/Users/karolinaryzka/Documents/huggingface-ptm-forking/huggingface-ptm-forking/huggingface_ptm_forking/originalBaseModel/README.md"
-    with open(filePath, 'r') as file:
-        fileContents = file.read()
-    result = getLinks(fileContents)
-    print("Extracted Links:")
-    for link in result:
-        print(link)
+
+    main()
+
     
     
     #rootDirectory = "Dataset"
